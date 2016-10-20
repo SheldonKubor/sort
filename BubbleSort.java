9950703008ff1a5f4db1c2f076dd45ddd9e389b7
mjh <mjh123877@163.com>
2016-10-20 20:38:17 +0800

    first push

---
+++ BubbleSort.java
@@ -0,0 +1,44 @@
+﻿public class BubbleSort{
+	public static void main(String[] args) {
+		int [] a={1,45,12,4,23,48,78};
+		System.out.println("排序前");
+		for (int i=0;i<a.length-1 ;i++ ) {
+			System.out.print(a[i]+"  ");
+		}
+		Sort.sort(a);
+		System.out.println();
+		for (int i=0;i<a.length-1 ;i++ ) {
+			System.out.print(a[i]+"  ");
+		}
+	}
+}
+class Sort{
+	public static void sort(int [] data){
+		for (int i=0;i<data.length-1 ;i++ ) {
+			for (int j=0;j<data.length-i-1 ;j++ ) {
+				if(data[j]>data[j+1]){
+					int temp = data[j];  
+                    data[j] = data[j + 1];  
+                    data[j + 1] = temp;
+				}
+			}
+		}
+	}
+}
+/*
+public class BubbleSort {  
+    public static void sort(int data[]) {  
+        for (int i = 0; i < data.length -1; i++) {  
+            for (int j = 0; j < data.length - i - 1; j++) {  
+                if (data[j] > data[j + 1]) {  
+                    int temp = data[j];  
+                    data[j] = data[j + 1];  
+                    data[j + 1] = temp;  
+                }  
+  
+            }  
+        }  
+    }  
+  
+}
+*/
---
+++ SelectSort.java
@@ -0,0 +1,57 @@
+/*
+public class SelectionSort {  
+    public static void sort(int data[]) {  
+        int minVal;  
+        int minIndex;  
+        for (int i = 0; i < data.length - 1; i++) {  
+            minVal = data[i];  
+            minIndex = i;  
+            for (int j = i + 1; j < data.length; j++) {  
+                if (data[j] < minVal) {  
+                    minVal = data[j];  
+                    minIndex = j;  
+                }  
+            }  
+            if (minVal != data[i] && minIndex != i) {  
+                data[minIndex] = data[i];  
+                data[i] = minVal;  
+            }  
+        }   
+    }  
+  } */
+  public class SelectSort{
+  	public static void main(String[] args) {
+  		int [] a={1,45,12,4,23,48,78};
+		System.out.println("before");
+		for (int i=0;i<a.length-1 ;i++ ) {
+			System.out.print(a[i]+"  ");
+		}
+		System.out.println();
+		Sort.sort(a);
+		System.out.println("after");
+		for (int i=0;i<a.length-1 ;i++ ) {
+			System.out.print(a[i]+"  ");
+		}
+  	}
+
+  }
+  class SelSort{
+  	public static void sort(int data[]) {  
+        int minVal;  
+        int minIndex;  
+        for (int i = 0; i < data.length - 1; i++) {  
+            minVal = data[i];  
+            minIndex = i;  
+            for (int j = i + 1; j < data.length; j++) {  
+                if (data[j] < minVal) {  
+                    minVal = data[j];  
+                    minIndex = j;  
+                }  
+            }  
+            if (minVal != data[i] && minIndex != i) {  
+                data[minIndex] = data[i];  
+                data[i] = minVal;  
+            }  
+        }   
+    }  
+  }
