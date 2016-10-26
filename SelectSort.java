4dee91a1eb48aad5d8ce19f95ec17d699233bdc7
SheldonKubor <mjh123877@163.com>
2016-10-26 09:35:01 +0800

    Update SelectSort.java

--- SelectSort.java
+++ SelectSort.java
@@ -1,24 +1,4 @@
-/*
-public class SelectionSort {  
-    public static void sort(int data[]) {  
-        int minVal;  
-        int minIndex;  
-        for (int i = 0; i < data.length - 1; i++) {  
-            minVal = data[i];  
-            minIndex = i;  
-            for (int j = i + 1; j < data.length; j++) {  
-                if (data[j] < minVal) {  
-                    minVal = data[j];  
-                    minIndex = j;  
-                }  
-            }  
-            if (minVal != data[i] && minIndex != i) {  
-                data[minIndex] = data[i];  
-                data[i] = minVal;  
-            }  
-        }   
-    }  
-  } */
+
   public class SelectSort{
   	public static void main(String[] args) {
   		int [] a={1,45,12,4,23,48,78};
